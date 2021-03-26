9dce1b8eeae3567a10bbe6230fc3bc8444d6b745
somaiabahaa93 <somaianaggar93@gmail.com>
2021-03-26 17:27:26 +0200

    staring client

--- projectLaravel/routes/web.php
+++ projectLaravel/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/rooms',function(){
+return view ("clients.rooms");
+});
+
+Route::get('/myreservation',function(){
+    return view ("clients.myReservation");
+    });
+    
