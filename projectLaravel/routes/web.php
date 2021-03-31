553cf9feb82016f2b8a62dc9755c7f5ffc6a3757
Rewan Naguib <rewan.mnaguib@gmail.com>
2021-03-31 15:52:57 +0200

    admin2

--- projectLaravel/routes/web.php
+++ projectLaravel/routes/web.php
@@ -39,11 +39,55 @@ Auth::routes();
 // });
 
 
+Route::view('/home', 'home')->middleware('auth');
+Route::view('/admin', 'admin');
+Route::view('/manager', 'manager');
+Route::view('/receptionist', 'receptionist');
+Route::view('/client', 'client');
+
+
+
+//manager Manager
+Route::get('/login/manager', [App\Http\Controllers\Auth\LoginController::class,'showManagerLoginForm']);
+Route::get('/register/manager', [App\Http\Controllers\Auth\RegisterController::class,'showManagerRegisterForm']);
+
+Route::post('/login/manager',[App\Http\Controllers\Auth\LoginController::class,'managerLogin']);
+Route::post('/register/manager', [App\Http\Controllers\Auth\RegisterController::class,'createManager']);
+
+//admin
+Route::get('/login/admin', [App\Http\Controllers\Auth\LoginController::class,'showAdminLoginForm']);
+Route::get('/register/admin', [App\Http\Controllers\Auth\RegisterController::class,'showAdminRegisterForm']);
+
+Route::post('/login/admin',[App\Http\Controllers\Auth\LoginController::class,'adminLogin']);
+Route::post('/register/admin', [App\Http\Controllers\Auth\RegisterController::class,'createAdmin']);
+
+
+ //receptionist
+ Route::get('/login/receptionist', [App\Http\Controllers\Auth\LoginController::class,'showReceptionistLoginForm']);    Route::get('/register/receptionist', [App\Http\Controllers\Auth\RegisterController::class,'showReceptionistRegisterForm']);
+
+    
+ Route::post('/login/receptionist',[App\Http\Controllers\Auth\LoginController::class,'receptionistLogin']);
+Route::post('/register/receptionist', [App\Http\Controllers\Auth\RegisterController::class,'createReceptionist']);
+ //client  Client
+
+ Route::get('/login/client', [App\Http\Controllers\Auth\LoginController::class,'showClientLoginForm']);
+ Route::get('/register/client', [App\Http\Controllers\Auth\RegisterController::class,'showClientRegisterForm']);
+ Route::post('/login/client',[App\Http\Controllers\Auth\LoginController::class,'clientLogin']);
+Route::post('/register/client', [App\Http\Controllers\Auth\RegisterController::class,'createClient']);
+
+
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
+
+
+
+
+
+
+
 //admin role
-<<<<<<< HEAD
+
 
 Route::get('/admins',function(){
     return view ("/admins/index" 
@@ -83,44 +127,11 @@ Route::get('/adminrooms/{room}/edit', [RoomController::class, 'edit'])->name('ad
 Route::put('/adminrooms/{room}', [RoomController::class, 'update'])->name('adminrooms.update');//->middleware('auth'); e::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update')->middleware('auth'); 
 Route::delete('/adminrooms/{room}', [RoomController::class, 'destroy'])->name('adminrooms.destroy');//->middleware('auth');                              
 Route::get('adminrooms/list', [RoomController::class, 'getRooms'])->name('adminrooms.list'); //Data table
-=======
-// Route::get('/admins',function(){
-//     return view ("/admins/index" 
-// );
-//     });
-//admin
-Route::get('/login/admin', [App\Http\Controllers\Auth\LoginController::class,'showAdminLoginForm']);
-Route::get('/register/admin', [App\Http\Controllers\Auth\RegisterController::class,'showAdminRegisterForm']);
-
-Route::post('/login/admin',[App\Http\Controllers\Auth\LoginController::class,'adminLogin']);
-Route::post('/register/admin', [App\Http\Controllers\Auth\RegisterController::class,'createAdmin']);
-
-//manager Manager
-    Route::get('/login/manager', [App\Http\Controllers\Auth\LoginController::class,'showManagerLoginForm']);
-    Route::get('/register/manager', [App\Http\Controllers\Auth\RegisterController::class,'showManagerRegisterForm']);
-    
-    Route::post('/login/manager',[App\Http\Controllers\Auth\LoginController::class,'managerLogin']);
-    Route::post('/register/manager', [App\Http\Controllers\Auth\RegisterController::class,'createManager']);
 
 
-    //receptionist
-    Route::get('/login/receptionist', [App\Http\Controllers\Auth\LoginController::class,'showReceptionistLoginForm']);
-    Route::get('/register/receptionist', [App\Http\Controllers\Auth\RegisterController::class,'showReceptionistRegisterForm']);
-    
-    Route::post('/login/receptionist',[App\Http\Controllers\Auth\LoginController::class,'receptionistLogin']);
-    Route::post('/register/receptionist', [App\Http\Controllers\Auth\RegisterController::class,'createReceptionist']);
-//client  Client
->>>>>>> 9fc94e4ca8e8720c1d350cead3ca05025f79d1fa
 
-    Route::get('/login/client', [App\Http\Controllers\Auth\LoginController::class,'showClientLoginForm']);
-    Route::get('/register/client', [App\Http\Controllers\Auth\RegisterController::class,'showClientRegisterForm']);
-    
-    Route::post('/login/client',[App\Http\Controllers\Auth\LoginController::class,'clientLogin']);
-    Route::post('/register/client', [App\Http\Controllers\Auth\RegisterController::class,'createClient']);
+//admin(floors)
 
-//admin(floors) 
-
-<<<<<<< HEAD
 Route::get('/adminfloors', [FloorController::class, 'index'])->name('adminfloors.index');//->middleware('auth');        
 Route::get('/adminfloors/create', [FloorController::class, 'create'])->name('adminfloors.create');//->middleware('auth');  
 Route::post('/adminfloors', [FloorController::class, 'store'])->name('adminfloors.store');//->middleware('auth');
@@ -128,20 +139,6 @@ Route::get('/adminfloors/{floor}/edit', [FloorController::class, 'edit'])->name(
 Route::put('/adminfloors/{floor}', [FloorController::class, 'update'])->name('adminfloors.update');//->middleware('auth'); e::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update')->middleware('auth'); 
 Route::delete('/adminfloors/{floor}', [FloorController::class, 'destroy'])->name('adminfloors.destroy');//->middleware('auth');                              
 Route::get('adminfloors/list', [FloorController::class, 'getFloors'])->name('adminfloors.list'); //Data table
-=======
-    Route::view('/home', 'home')->middleware('auth');
-    Route::view('/admin', 'admin');
-    Route::view('/manager', 'manager');
-    Route::view('/receptionist', 'receptionist');
-    Route::view('/client', 'client');
-    
-// Route::get('/admins', [AdminController::class, 'index'])->name('admins.index');
-// Route::get('/receptionists', [ReceptionistController::class, 'index'])->name('receptionists.index');//->middleware('auth');        
-// Route::get('/receptionists/create', [ReceptionistController::class, 'create'])->name('receptionists.create');//->middleware('auth');  
-// Route::post('/receptionists', [ReceptionistController::class, 'store'])->name('receptionists.store');//->middleware('auth');
-// Route::get('/receptionists/{receptionist}/edit', [ReceptionistController::class, 'edit'])->name('receptionists.edit');//->middleware('auth'); 
-// Route::put('/receptionists/{receptionist}', [ReceptionistController::class, 'update'])->name('receptionists.update');//->middleware('auth'); e::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update')->middleware('auth'); 
-// Route::delete('/receptionists/{receptionist}', [ReceptionistController::class, 'destroy'])->name('receptionists.destroy');//->middleware('auth'); 
-// //Route::get('/receptionists/delete/{id}', 'ReceptionistController@destroy')->name('receptionists.destroy');
-// Route::get('receptionists/list', [ReceptionistController::class, 'getReceptionists'])->name('receptionists.list'); //Data table
->>>>>>> 9fc94e4ca8e8720c1d350cead3ca05025f79d1fa
+
+  
+
